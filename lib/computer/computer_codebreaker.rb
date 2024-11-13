de69f3454231d46b494299337aae39343e376a0f
Altervl <62514019+Altervl@users.noreply.github.com>
2024-11-13 22:57:22 +0300

    Add a method that saves all possible codes
    Part of computer player logic

--- lib/computer/computer_codebreaker.rb
+++ lib/computer/computer_codebreaker.rb
@@ -4,6 +4,10 @@ require 'colorize'
 
 # Class for computer player as a codebreaker in the Mastermind game
 class ComputerCodebreaker
+  def initialize
+    @combos = generate_combos
+  end
+
   def decipher(colors, cue)
     guess = []
     4.times do
@@ -12,5 +16,29 @@ class ComputerCodebreaker
     end
 
     guess
+  end
+
+  private
+
+  attr_reader :combos
+
+  # Recursive method that returns all possible code combinations
+  def generate_combos(colors, length, current = [], all = [])
+    # Base case
+    if current.size == length
+      all << current.dup # save a copy of the combo
+      return all # back to previous level of recursion
+    end
+
+    # Recursive case
+    colors.each do |color|
+      current << color
+      # Call the method itself with array of colors
+      generate_combos(colors, length, current, all)
+      current.pop # delete last inserted color to insert next
+    end
+
+    # Return all possible combos
+    all
   end
 end
